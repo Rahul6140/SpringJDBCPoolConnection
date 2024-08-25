03c53c0e01b96d68b3d4c6b2a03937ac03a4ab50
Rahul Yadav <rahuly.yadav0@gmail.com>
2024-08-26 00:14:34 +0530

    Connection Pool

---
+++ src/main/java/com/pool/SpringJdbcApplication.java
@@ -0,0 +1,36 @@
+package com.pool;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication(exclude = {JdbcTemplateAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class SpringJdbcApplication implements CommandLineRunner {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application Started");
+		List<Map<String, Object>> maps = jdbcTemplate.queryForList("Select * from public.\"Student\"");
+
+		maps.forEach(items ->{
+			System.out.println("Id : "+items.get("ID"));
+			System.out.println("Name : "+items.get("Name"));
+			System.out.println("Age : "+items.get("Age"));
+			System.out.println("---------------------------------");
+		});
+
+	}
+}
